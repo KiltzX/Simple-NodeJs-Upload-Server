721fc1314b4bf66bb2d8dac572eac0144ebb1214
Kiltzx <dudu.cedsv@gmail.com>
2023-10-26 17:40:23 -0300

    progress bar

--- app.js
+++ app.js
@@ -1,24 +1,10 @@
 const express = require('express');
-const multer = require('multer');
+const fileUpload = require('express-fileupload');
 const path = require('path');
 const fs = require('fs');
 const app = express();
 
-// Configuração para armazenar os arquivos enviados
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    const uploadPath = 'uploads/' + req.body.folderName; // Use o nome da pasta fornecido no corpo da solicitação
-    if (!fs.existsSync(uploadPath)) {
-      fs.mkdirSync(uploadPath, { recursive: true });
-    }
-    cb(null, uploadPath);
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-  },
-});
-
-const upload = multer({ storage: storage });
+app.use(fileUpload());
 
 // Define a pasta de arquivos estáticos
 app.use(express.static('public'));
@@ -27,11 +13,33 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/upload', upload.array('files'), (req, res) => {
+app.post('/upload', (req, res) => {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('Nenhum arquivo enviado.');
+  }
+
+  // Pasta de destino para o upload
+  const uploadPath = path.join(__dirname, 'uploads', req.body.folderName);
+
+  if (!fs.existsSync(uploadPath)) {
+    fs.mkdirSync(uploadPath, { recursive: true });
+  }
+
+  // Processar os arquivos enviados
+  for (let file of Object.values(req.files.files)) {
+    const fileName = file.name;
+    const filePath = path.join(uploadPath, fileName);
+
+    file.mv(filePath, (err) => {
+      if (err) {
+        return res.status(500).send(err);
+      }
+    });
+  }
+
   res.send('Arquivos enviados com sucesso');
 });
 
 app.listen(3000, () => {
   console.log('Servidor rodando em http://localhost:3000');
 });
-
